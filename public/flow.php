9efb09f4773f8d58e9031460062f18250417648b
Faventyne <guitchat@gmail.com>
2017-11-21 16:39:21 +0100

    commit eod

--- public/flow.php
+++ public/flow.php
@@ -1,8 +1,8 @@
 <?php
 require_once __DIR__ .'/../inc/config.php';
 
-
-
+$sql="INSERT INTO opportunity(opp_link,opp_intref,opp_title,opp_dept,opp_company,opp_sector,opp_location,opp_stage,opp_proba,opp_creadate,opp_enddate,opp_candidates,user_sur_id)
+VALUES (:link,:intref,:title,:dept,:company,:sector,:location,:stage,:proba,:creadate,:enddate,:candidated,".$_SESSION['id'].")";
 
 require_once __DIR__ .'/../view/header.php';
 require_once __DIR__ .'/../view/flow.php';
