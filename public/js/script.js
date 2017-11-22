27bcce239c1a9510560717e5773a171f4e39aa1e
Faventyne <guitchat@gmail.com>
2017-11-22 17:00:03 +0100

    commit eod

--- public/js/script.js
+++ public/js/script.js
@@ -15,15 +15,17 @@
     }
 
     function isCorrectDate(date,type){
-        var myDate = date.split("/");
-        var d = parseInt(date[0],10);
-        var m = parseInt(date[1],10);
-        var y = parseInt(date[2],10);
+        var myDate = date.split("-");
+        var d = parseInt(myDate[2],10);
+        var m = parseInt(myDate[1],10);
+        var y = parseInt(myDate[0],10);
         //new Date(y,m-1,d);
         if(type=="opp-enddate"){
-            return (new Date(y,m-1,d) - new Date()) > 180;
+            return (new Date(y,m-1,d).getTime() - new Date().getTime()) < 180*24*3600*1000;
         } else if(type == "opp-startdate"){
-            return (new Date(y,m-1,d) - new Date()) <= 0;
+            var inputDate = new Date(y,m-1,d);
+            var today = new Date();
+            return (new Date(y,m-1,d).getTime() - new Date().getTime()) <= 0;
         }
     }
 
@@ -59,27 +61,45 @@
         }
     });
 
+    $('[name="opp-startdate"]').on("click",function(){
+        if($('[name="opp-startdate"]').css('border-color')=='rgb(255, 0, 0)'){
+            $('#datestartHelp').removeClass("data-error").addClass("text-muted");
+            $('#datestartHelp').text("Leave blank if unknown");
+            $('[name="opp-startdate"]').css('border-color','');
+
+        }
+    });
+
+    $('[name="opp-enddate"]').on("click",function(){
+        if($('[name="opp-enddate"]').css('border-color')=='rgb(255, 0, 0)'){
+            $('#dateendHelp').removeClass("data-error").addClass("text-muted");
+            $('#dateendHelp').text("Leave blank if unknown");
+            $('[name="opp-enddate"]').css('border-color','');
+
+        }
+    });
+
     //Form errors
     $('[name="opp-submit"]').on("click",function(e){
       e.preventDefault();
       if(!(isNonZeroUnitProba($('[name="opp-successrate"]').val()) || isNonZeroIntProba($('[name="opp-successrate"]').val()))){
-          if(!$('#rateHelp').length){
+          if(!$('#successrateHelp').length){
               $('#successRate').append('<small id="successrateHelp" class="form-text data-error">Please insert a probability between 0 and 100%</small>');
               $('[name="opp-successrate"]').css("border-color","red");
           }
       }
 
-      if(!(isNonZeroInt($('[name="opp-candidates"]').val()))){
+      if(!$('[name="opp-candidates"]').val() == "" && !(isNonZeroInt($('[name="opp-candidates"]').val()))){
           $('#candidatesHelp').removeClass("text-muted").addClass("data-error").text('Please insert a valid number of remaining candidates');
           $('[name="opp-candidates"]').css("border-color","red");
       }
 
-      if(!$('[name="opp-enddate"]').val() == "" && !isCorrectDate($('[name="opp-enddate"]').val()),"opp-enddate"){
+      if(!$('[name="opp-enddate"]').val() == "" && !isCorrectDate($('[name="opp-enddate"]').val(),"opp-enddate")){
           $('#dateendHelp').removeClass("text-muted").addClass("data-error").text('The opportunity cannot expire after 6 months, choose a more recent date');
           $('[name="opp-enddate"]').css("border-color","red");
       }
 
-      if(!$('[name="opp-startdate"]').val() == "" && !isCorrectDate($('[name="opp-startdate"]').val()),"opp-startdate"){
+      if(!$('[name="opp-startdate"]').val() == "" && !isCorrectDate($('[name="opp-startdate"]').val(),"opp-startdate")){
           $('#datestartHelp').removeClass("text-muted").addClass("data-error").text('The opportunity creation date should be today or earlier');
           $('[name="opp-startdate"]').css("border-color","red");
       }
